c1e3e63db69fc8bfb142f4a4ea110c0b25a99364
Rajvir <rajvirz2004@gmail.com>
2022-11-20 00:32:32 -0700

    the new dict

--- NGOS.py
+++ NGOS.py
@@ -30,26 +30,24 @@ CAR_NGO = {
 "AFDP Globsl Foundation": ['sports'],
 "International Shadow Piece Force": ['war', 'refugees'],
 "International Wildlife Bond": ['animal'],
-"Central African Organization": ['homelessness', 'electricity']
+"Central African Organization": ['homelessness', 'electricity'],
+'WWF': ['pollution'], 
+'SISTERS of CHARITY of ST JEANNE ANTIDE CIO': ['homeless', 'religion'],
+'CHURCH MISSION SOCIETY': ['religion', 'cultural divides'],
+'WORLD LAND TRUST': ['pollution'],
+'YOUTH BUSINESS INTERNATIONAL': ['business'], # economy
+'OPERATIONAL SMILE UNITED KINGDOM': ['healthcare'],
+'FAMILY for EVERY CHILD': ['education'],
+'GLOBAL CANOPY': ['pollution'],
+'TEACH A MAN TO FISH': ['business'],
+'THE GLOBAL WARMING POLICY FOUNDATION': ['pollution'],
+'THE SLOTH CONSERVATION FOUNDATION': ['education', 'shelter', 'pollution'],
+'GOOD NEIGHBOURS UK': ['human rights'],
+'THE INTERNATIONAL CML FOUNDATION': ['diseases'],
+'WATER SURVIVAL BOX LTD': ['water'],
+'MISSIONARY SISTERS OF THE SACRED HEART': ['education', 'healthcare', 'poverty'],
+'SANTA MARIA EDUCATION FUND': ['education', 'poverty'],
+'NEOTOPICAL BIRD CLUB': ['wildlife'],
+'ARCAID': ['natural disasters']
 }
 
-global_NGO = {
-            'WWF': ['pollution'], 
-            'SISTERS of CHARITY of ST JEANNE ANTIDE CIO': ['homeless', 'religion'],
-            'CHURCH MISSION SOCIETY': ['religion', 'cultural divides'],
-            'WORLD LAND TRUST': ['pollution'],
-            'YOUTH BUSINESS INTERNATIONAL': ['business'], # economy
-            'OPERATIONAL SMILE UNITED KINGDOM': ['healthcare'],
-            'FAMILY for EVERY CHILD': ['education'],
-            'GLOBAL CANOPY': ['pollution'],
-            'TEACH A MAN TO FISH': ['business'],
-            'THE GLOBAL WARMING POLICY FOUNDATION': ['pollution'],
-            'THE SLOTH CONSERVATION FOUNDATION': ['education', 'shelter', 'pollution'],
-            'GOOD NEIGHBOURS UK': ['human rights'],
-            'THE INTERNATIONAL CML FOUNDATION': ['diseases'],
-            'WATER SURVIVAL BOX LTD': ['water'],
-            'MISSIONARY SISTERS OF THE SACRED HEART': ['education', 'healthcare', 'poverty'],
-            'SANTA MARIA EDUCATION FUND': ['education', 'poverty'],
-            'NEOTOPICAL BIRD CLUB': ['wildlife'],
-            'ARCAID': ['natural disasters']
-}
---
+++ ngodict.py
@@ -0,0 +1,22 @@
+ngodict = { 'WWF': ['pollution'], 
+            'SISTERS of CHARITY of ST JEANNE ANTIDE CIO': ['homeless', 'religion'],
+            'CHURCH MISSION SOCIETY': ['religion', 'cultural divides'],
+            'WORLD LAND TRUST': ['pollution'],
+            'YOUTH BUSINESS INTERNATIONAL': ['business'], # economy
+            'OPERATIONAL SMILE UNITED KINGDOM': ['healthcare'],
+            'FAMILY for EVERY CHILD': ['education'],
+            'GLOBAL CANOPY': ['pollution'],
+            'TEACH A MAN TO FISH': ['business'],
+            'THE GLOBAL WARMING POLICY FOUNDATION': ['pollution'],
+            'THE SLOTH CONSERVATION FOUNDATION': ['education', 'shelter', 'pollution'],
+            'GOOD NEIGHBOURS UK': ['human rights'],
+            'THE INTERNATIONAL CML FOUNDATION': ['diseases'],
+            'WATER SURVIVAL BOX LTD': ['water'],
+            'MISSIONARY SISTERS OF THE SACRED HEART': ['education', 'healthcare', 'poverty'],
+            'SANTA MARIA EDUCATION FUND': ['education', 'poverty'],
+            'NEOTOPICAL BIRD CLUB': ['wildlife'],
+            'ARCAID': ['natual disasters']
+            
+
+            
+            }  
