389ec4a61f0b816e281e795e6e84276012047a0a
devAdhiraj <adhiraj.bal@gmail.com>
2022-11-20 13:07:42 -0500

    added partner logos + descriptions

---
+++ temp.py
@@ -0,0 +1,108 @@
+CAR_NGO = {
+"ISLAMIC RELIEF WORLDWIDE": ['education', 'healthcare', 'water'],
+"INTERNATIONAL RESCUE COMMITTEE, U.K": ['homelessness', 'electricity'],
+"INTERNATIONAL MEDICAL CORPS (UK)": ['healthcare'],
+"EDUCATION DEVELOPMENT TRUST": ['education'],
+"WORLD VISION UK": ['homelessness', 'electricity'],
+"CATHOLIC AGENCY FOR OVERSEAS DEVELOPMENT": ['poverty'],
+"CONCERN WORLDWIDE (UK)": ['hunger', 'water'],
+"PENNY APPEAL": ['water', 'hunger', 'healthcare'],
+"SISTERS OF CHARITY OF ST JEANNE ANTIDE CIO": ['homeless', 'education'],
+"WAR CHILD": ['orphans'],
+"ONE NATION": ['orphans'],
+"CONCILIATION RESOURCES": [''],
+"A C O R D": ['electricity', 'homelessness', 'water', 'hunger'],
+"THE BORN FREE FOUNDATION": ['poaching', 'wildlife'],
+"Inspiration Incubator": ['community ', 'society'],
+"Everlasting Foundation": ['hunger', 'clothes', 'education'],
+"Conserve Global": ['animal'],
+"Center fior civilians in conflict(UK)": ['education'],
+"Muslim futures": ['education', 'minority'],
+"Uptime Catalyst facility": ['water'],
+"International Tanaba(ITANABA)": ['poverty', 'animal'],
+"The Arcanum foundation": ['youth clubs', 'animal', 'veterans'],
+"Kariba Health Organizatiion": ['healthcare'],
+"African Rights Action": ['inclusion', 'refugees'],
+"International Welfare Allience(IWARELIEF)": ['healthcare', 'hunger', 'clothing', 'homelessness', 'electricity'],
+"KD Foundation": ['climate',  'hunger' , 'clothing', 'education', 'healthcare'],
+"Smart": ['hunger'],
+"Thirdway Trust": ['education'],
+"AFDP Globsl Foundation": ['sports'],
+"International Shadow Piece Force": ['war', 'refugees'],
+"International Wildlife Bond": ['animal'],
+"Central African Organization": ['homelessness', 'electricity'],
+            'WWF': ['pollution'], 
+            'SISTERS of CHARITY of ST JEANNE ANTIDE CIO': ['homeless', 'religion'],
+            'CHURCH MISSION SOCIETY': ['religion', 'cultural divides'],
+            'WORLD LAND TRUST': ['pollution'],
+            'YOUTH BUSINESS INTERNATIONAL': ['business'], # economy
+            'OPERATIONAL SMILE UNITED KINGDOM': ['healthcare'],
+            'FAMILY for EVERY CHILD': ['education'],
+            'GLOBAL CANOPY': ['pollution'],
+            'TEACH A MAN TO FISH': ['business'],
+            'THE GLOBAL WARMING POLICY FOUNDATION': ['pollution'],
+            'THE SLOTH CONSERVATION FOUNDATION': ['education', 'shelter', 'pollution'],
+            'GOOD NEIGHBOURS UK': ['human rights'],
+            'THE INTERNATIONAL CML FOUNDATION': ['diseases'],
+            'WATER SURVIVAL BOX LTD': ['water'],
+            'MISSIONARY SISTERS OF THE SACRED HEART': ['education', 'healthcare', 'poverty'],
+            'SANTA MARIA EDUCATION FUND': ['education', 'poverty'],
+            'NEOTOPICAL BIRD CLUB': ['wildlife'],
+            'ARCAID': ['natural disasters']
+}
+
+for k in CAR_NGO.keys():
+  print(f"<li>{k.capitalize()}</li>")
+
+'''
+<li>Islamic relief worldwide</li>
+<li>International rescue committee, u.k</li>
+<li>International medical corps (uk)</li>
+<li>Education development trust</li>
+<li>World vision uk</li>
+<li>Catholic agency for overseas development</li>
+<li>Concern worldwide (uk)</li>
+<li>Penny appeal</li>
+<li>Sisters of charity of st jeanne antide cio</li>
+<li>War child</li>
+<li>One nation</li>
+<li>Conciliation resources</li>
+<li>A c o r d</li>
+<li>The born free foundation</li>
+<li>Inspiration incubator</li>
+<li>Everlasting foundation</li>
+<li>Conserve global</li>
+<li>Center fior civilians in conflict(uk)</li>
+<li>Muslim futures</li>
+<li>Uptime catalyst facility</li>
+<li>International tanaba(itanaba)</li>
+<li>The arcanum foundation</li>
+<li>Kariba health organizatiion</li>
+<li>African rights action</li>
+<li>International welfare allience(iwarelief)</li>
+<li>Kd foundation</li>
+<li>Smart</li>
+<li>Thirdway trust</li>
+<li>Afdp globsl foundation</li>
+<li>International shadow piece force</li>
+<li>International wildlife bond</li>
+<li>Central african organization</li>
+<li>WWF</li>
+<li>Sisters of charity of st jeanne antide cio</li>
+<li>Church mission society</li>
+<li>World land trust</li>
+<li>Youth business international</li>
+<li>Operational smile united kingdom</li>
+<li>Family for every child</li>
+<li>Global canopy</li>
+<li>Teach a man to fish</li>
+<li>The global warming policy foundation</li>
+<li>The sloth conservation foundation</li>
+<li>Good neighbours uk</li>
+<li>The international cml foundation</li>
+<li>Water survival box ltd</li>
+<li>Missionary sisters of the sacred heart</li>
+<li>Santa maria education fund</li>
+<li>Neotopical bird club</li>
+<li>Arcaid</li>
+'''
